fae2221ec7ad41e620bf13d9c37057f176e25aec
v14dis14v <vladislav.reshetow@yandex.ru>
2019-08-15 00:03:43 +0300

    1.9 done

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['\r\n'.join(environ['QUERY_STRING'].split('&'))]
+def wsgi_application(environ, start_response):
+    # бизнес-логика
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    query = environ['QUERY_STRING']
+    start_response(status, headers )
+    return [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    #return environ['QUERY_STRING'].split('&') 
--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-sudo apt update
-sudo apt install mc
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart hello
+#sudo /etc/init.d/gunicorn restart /home/box/web/etc/gunicorn.conf
+#sudo /etc/init.d/gunicorn -c /etc/gunicorn.d/test hello
+# sudo /etc/init.d/mysql start
+gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
