a7aad2e3488db44efcbaf4d7a019b88ba224236d
v14dis14v <vladislav.reshetow@yandex.ru>
2019-08-14 22:31:28 +0300

    project 1.9.11 done

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo apt update
+sudo apt install mc
